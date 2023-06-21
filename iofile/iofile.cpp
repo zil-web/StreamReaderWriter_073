64eb0a12e3228ec529ee90e64bc7254b97a3a5a8
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-21 10:27:27 +0700

    menulis dan memasukkan nilai dari 'baris' ke dalam file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -12,4 +12,13 @@ int  main() {
 
 	cout << ">= menulis file, \'q'\ untuk keluar " << endl;
 	// unlimited loop untuk menulis
+	while (true) {
+		cout << "_ ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akanberhenti jika anada memasukkan karakter q
+		if (baris == "q") break;
+		// menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
