ad3e6d3d4ff7f39dce8ee2f2edad967d026d0a11
zil-web <126854462+zil-web@users.noreply.github.com>
2023-06-21 10:36:50 +0700

    jika file ada maka

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -21,4 +21,18 @@ int  main() {
 		// menulis dan memasukkan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
+	// selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuakkan ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= membuka dan membaca file" << endl;
+	// jika file ada maka
+	if (infile.is_open())
+	{
+		//
+	}
 }
